480ec0011c964033b0295c98e1609696edfab74d
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-11-06 21:38:40 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,5 +1,10 @@
-#in operator membership expression.
-print('in operator membership expression')
+#Generating powers-of-2 list L
+L = []
+for i in range(6):
+	L.append(2**i) 
+
+print(L)
+X=5
 if 2**X in L:
 	print('at index',L.index(2**X))
 else:
