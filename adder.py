ed40bd9a8f8b3344a38093253430636257edc2f6
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-11-06 21:49:07 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -1,8 +1,8 @@
 def adder(x,y):
 	return(x+y)
 
-#addition of two list
+#addition of two floating points
 
-L1 = [1,'mubai',12.45 ]
-L2 = ['1', 'TIFR','@123']
-print(adder(L1,L2)) #we need  print statement
+F1 = 10.23
+F2 = 11.4
+print(adder(F1,F2)) #we need  print statement
