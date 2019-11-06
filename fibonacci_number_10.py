742f5417fd058777b3ec6708f7f561df98692ea1
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-11-06 21:14:47 +0530

    Update fibonacci_number_10.py

--- fibonacci_number_10.py
+++ fibonacci_number_10.py
@@ -1,22 +1,6 @@
-#module
-def fib(n):
+#100 fibonacci number
+import module_fib
 
-	if n<0:
-		print("Please enter a postive integer...")
-	
-	elif n==0:
-		print(0)
+N = int(input("Enter number of fibonacci number: ") )
 
-	elif n>0:
-		a = 0
-		b = 1
-
-		print(a)
-
-		for i in range(n-1):
-			c = a+b
-			print(c)
-			a=b
-			b=c	
-	return
-
+module_fib.fib(N)
