427945881183a587caafd435dfe49ccfed2ec58f
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-11-06 21:35:06 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,8 +1,6 @@
-#list index method
-print('list index method')
-for num in L:
-	if 2**X == num:
-		print('at index',L.index(num))
-		break
+#in operator membership expression.
+print('in operator membership expression')
+if 2**X in L:
+	print('at index',L.index(2**X))
 else:
 	print(X, 'not found')
