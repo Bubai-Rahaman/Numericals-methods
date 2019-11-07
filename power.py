5b76b25c126f645f1839d8bdcd56cba7135da4c2
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-11-07 11:07:31 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,12 +1,10 @@
 L = [1,2,4,8,16,32,64]
 X = 5
 
-#for-else construction
-for i in range(len(L)):
-	if 2**X == L[i]:
-		print('at index', i)
+#list index method
+for num in L:
+	if 2**X == num:
+		print('at index',L.index(num))
 		break
-	else:
-		i = i+1
 else:
 	print(X, 'not found')
