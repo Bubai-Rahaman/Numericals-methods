63365628822b56da17636e5b86bc186e721d5df3
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-11-07 11:06:09 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,10 +1,8 @@
 L = [1,2,4,8,16,32,64]
 X = 5
 
-#while-else construction
-i=0
-
-while i < len(L):
+#for-else construction
+for i in range(len(L)):
 	if 2**X == L[i]:
 		print('at index', i)
 		break
