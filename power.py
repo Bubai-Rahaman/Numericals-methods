ee24a29c349b22baac6146e4a2374a9d6ccd7d59
Bubai-Rahaman <57394553+Bubai-Rahaman@users.noreply.github.com>
2019-11-07 11:10:12 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,7 +1,12 @@
-L = [1,2,4,8,16,32,64]
-X = 5
+#Generating powers-of-2 list L
+L = []
+for i in range(6):
+	L.append(2**i) 
 
-#in operator membership expression.
+print(L)
+
+X=5
+
 if 2**X in L:
 	print('at index',L.index(2**X))
 else:
